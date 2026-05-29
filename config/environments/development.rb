06701229e6aa8a32fcc2f7b49bc364be37987c7d
cassiemai <cassie_mai07@outlook.com>
2026-05-29 11:41:24 -0500

    ready for railway deployment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,6 +25,9 @@ Rails.application.configure do
     config.action_controller.perform_caching = false
   end
 
+  # allow ngrok to make requests
+  config.hosts << "atlas-cassie.ngrok.app"
+
   # Change to :null_store to avoid any caching.
   config.cache_store = :memory_store
 
